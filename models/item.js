60c8fc9afa1675affa9448b84c7f88de575a196b
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2021-01-20 21:46:53 +1030

    update item model with more details

--- models/item.js
+++ models/item.js
@@ -11,11 +11,56 @@ const ItemSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    // sold: {
-    //     type: Boolean,
-    //     required: true,
-    //     default: false
-    // }
+    soldOut: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    // images
+    imageMain: {
+        type: String,
+        default: "https://via.placeholder.com/400",
+        required: true
+    },
+    imageTwo: {
+        type: String,
+        default: "https://via.placeholder.com/400",
+        required: true
+    },
+    imageThree: {
+        type: String,
+        default: "https://via.placeholder.com/400",
+        required: true
+    },
+    imageFour: {
+        type: String,
+        default: "https://via.placeholder.com/400",
+        required: true
+    },
+    sizeWidthCM: {
+        type: String,
+        required: true
+    },
+    sizeLengthCM: {
+        type: String,
+        required: true
+    },
+    sizeWidthInch: {
+        type: String,
+        required: true
+    },
+    sizeLengthInch: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = Item = mongoose.model('item', ItemSchema);
