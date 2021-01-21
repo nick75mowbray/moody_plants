fab78d5df7ede269c194d1f68b48fab5765745eb
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2021-01-21 14:12:47 +1030

    add home page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Nav from './components/Nav';
+// pages
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
     <div>
       <Nav/>
-      <h1>Hello</h1>
+      <Route exact path="/">
+        <Home/>
+        </Route>
+      <Route path="/about"></Route>
+      <Route path="/account"></Route>
+      <Route path="/cart"></Route>
+      <Route path="/login"></Route>
     </div>
     </Router>
   );
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageList from '../components/ImageList';
+import Hero from '../components/Hero';
+
+function Home() {
+    return (
+        <>
+            <Hero/>
+            <ImageList/>
+        </>
+    )
+}
+
+export default Home;
